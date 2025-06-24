899d142b6e92443956679c4302de5076f77f01b3
parvejtalukder <mail@parvejhusentalukder.com>
2025-06-24 12:36:02 +0600

    new inclution

--- BeeCrowd/Beginner/1150.c
+++ BeeCrowd/Beginner/1150.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 int main() {
-    int base_number, to_number, i;
-    int co_number = 0, sum_number = 0;
-    scanf("%d", &)
-    return 0;
+    int X, Z, sum = 0, count = 0;
+    scanf("%d", &X);
+    do {
+        scanf("%d", &Z);
+    } while (Z <= X);
+
+    int i = X;
+    while (sum < Z) {
+        sum += i;
+        count++;
+        i++;
+    }
+    printf("%d\n", count);
+    return 0;   
 }
--- Random/String/string_prac.c
+++ Random/String/string_prac.c
@@ -5,4 +5,4 @@ int main () {
         printf("%s\n", ara);
     }
     return 0;
-}
+} 
