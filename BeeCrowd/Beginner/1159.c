d6e4557bfa591cf65169b83d61d20c3c4d151957
parvejtalukder <mail@parvejhusentalukder.com>
2025-06-26 20:01:22 +0600

    done

--- BeeCrowd/Beginner/1159.c
+++ BeeCrowd/Beginner/1159.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-// My CODE BY PHT
 int main() {
     int x, count, sum, i;
     while (1) {
@@ -19,23 +17,3 @@ int main() {
     }
     return 0;
 }
-=======
-int main() {
-  int x, sum = 0, count = 0;
-  do {
-  scanf("%d", &x);
-  int t = x;
-  while(count <= 5) {
-  if (t % 2 == 0) {
-  sum = sum + t;
-  count++;
-  }
-  t++;
-  }
-  printf("%d\n", sum);
-  }
-  while (x > 0);
-  
-  return 0;
-}
->>>>>>> 526d7e5001d22427669004fdaab7182dfb0a1289
