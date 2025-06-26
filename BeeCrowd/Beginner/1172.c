60ee9368e580103a53d833bba24d79408f7ad3ed
parvejtalukder <mail@parvejhusentalukder.com>
2025-06-26 21:36:37 +0600

    push

---
+++ BeeCrowd/Beginner/1172.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int x[10];
+    int i, j;
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &x[i]);
+        if (x[i] <= 0) {
+            x[i] = 1;
+        }
+    }
+    for(j = 0; j < 10; j++) {
+        printf("X[%d] = %d\n", j, x[j]);
+    }
+    return 0;
+}
