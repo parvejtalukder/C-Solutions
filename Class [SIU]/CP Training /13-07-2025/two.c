a9028826dd0662b66fd61b61e57abd03cedae691
parvejtalukder <mail@parvejhusentalukder.com>
2025-07-13 14:24:11 +0600

    inclugn this is;

---
+++ Class [SIU]/CP Training /13-07-2025/two.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int row, colm;
+    scanf("%d %d", &row, &colm);
+    int arr[row][colm];
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < colm; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colm; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
