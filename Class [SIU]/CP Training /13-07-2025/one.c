d6c85c7ba1ad89075c6c02048eafc5feea957d99
parvejtalukder <mail@parvejhusentalukder.com>
2025-07-13 14:01:16 +0600

    including new files

--- Class [SIU]/CP Training /13-07-2025/one.c
+++ Class [SIU]/CP Training /13-07-2025/one.c
@@ -2,12 +2,12 @@
 int main()
 {
     int arr[10];
-    int size = 10;
+    int size;
+    scanf("%d", &size);
     for(int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
-    for(int i = 0;i < 10; i++) {
-        printf("arr[%d] is: ", i);
+    for(int i = 0;i < size; i++) {
         printf("%d\n", arr[i]);
     }
     return 0;
