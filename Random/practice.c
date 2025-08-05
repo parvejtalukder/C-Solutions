a378263430f7e0157bc9a9f34a5955cc63452ae1
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-05 19:57:59 +0600

    Update: Files

---
+++ Random/practice.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[12][12];
+    for (int i = 0; i < 12; i++) {
+        for(int j = 0; j < 12; j++) {
+            arr[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < 12; i++) {
+        for(int j = 0; j < 12; j++) {
+            printf(" %d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
