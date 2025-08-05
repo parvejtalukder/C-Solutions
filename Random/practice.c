193eecc98534e5600e05104fb37e92359a1e6efc
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-05 20:04:17 +0600

    New FIles

--- Random/practice.c
+++ Random/practice.c
@@ -4,10 +4,17 @@ int main() {
     int arr[12][12];
     for (int i = 0; i < 12; i++) {
         for(int j = 0; j < 12; j++) {
+            // int a = 0, b = 0;
+            // arr[a][b] = 1;
+            // a++;
+            // b++;
             arr[i][j] = 0;
+            if (i == j) {
+                arr[i][j] = 1;
+            }
         }
     }
-    
+
     for (int i = 0; i < 12; i++) {
         for(int j = 0; j < 12; j++) {
             printf(" %d ", arr[i][j]);
