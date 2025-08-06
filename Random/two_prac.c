c95f4d08d8512fb4aedaa3bb8087a041e5233782
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-06 19:06:49 +0600

    Update: Files

--- Random/two_prac.c
+++ Random/two_prac.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-
 int main() {
     int arr[12][12];
-
     for (int i = 0; i < 12; i++) {
         for(int j = 0; j < 12; j++) {
             arr[i][j] = 0;           
-            int x, y;
-            for(x = i+1; x < 12; x++) {
+            if (i == j) {
+                arr[i][j] = 1;       
+            } 
+            if (i + j == 11) {
+                arr[i][j] = 1;
+            }
+            int x;
+            for(x = i+1; x < 12 - (i+1); x++) {
                 arr[i][x] = 5;
             }
         }
     }
-
     for (int i = 0; i < 12; i++) {
         for(int j = 0; j < 12; j++) {
             printf("%2d ", arr[i][j]);  
         }
         printf("\n");
     }
-
     return 0;
 }
