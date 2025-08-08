a521223cd5f328127649cdd261f937fed6354105
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-08 19:57:01 +0600

    Update: Files

--- Pointers/Struct Pointers/a.c
+++ Pointers/Struct Pointers/a.c
@@ -1,1 +1,16 @@
 // Struct 
+struct Car {
+  char brand[50];
+  int year;
+};
+
+int main() {
+  struct Car car = {"Toyota", 2020};
+
+  struct Car *ptr = &car;
+
+  printf("Brand: %s\n", ptr->brand);
+  printf("Year: %d\n", ptr->year);
+
+  return 0;
+}
